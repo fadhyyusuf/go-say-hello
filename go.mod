eed759fd373774f3f5484c98cb032586f1434613
fadhyyusuf <fadhy.yusuf@bithealth.co.id>
2025-12-23 14:47:04 +0700

    add more param

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/fadhyyusuf/go-say-hello
+module github.com/fadhyyusuf/go-say-hello/v2
 
 go 1.25.5
--- say_hello.go
+++ say_hello.go
@@ -4,6 +4,6 @@ func SayHello(name string) string {
 	return "Hello, " + name
 }
 
-func SayHelloWorld() string {
-	return "Hello, world"
+func SayHelloWorld(name string) string {
+	return "Hello, world" + name
 }
